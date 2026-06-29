10664d63943ce0fd4c7511bbd655c5a0905f495c
Shreyansh Singh <singhshreyansh288@gmail.com>
2026-06-29 15:04:30 +0530

    added analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,21 @@
-import './globals.css';
-import Navbar from './components/navbar';
-import Cursor from './components/cursor';
+import "./globals.css";
+import Navbar from "./components/navbar";
+import Cursor from "./components/cursor";
+import { Analytics } from "@vercel/analytics/next";
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-    return (
-        <html lang='en'>
-            <body>
-                <Cursor />
-                <Navbar />
-                {children}
-            </body>
-        </html>
-    )
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <Analytics />
+        <Cursor />
+        <Navbar />
+        {children}
+      </body>
+    </html>
+  );
 }
