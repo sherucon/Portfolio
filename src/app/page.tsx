8de32893143b3ded8389feda1d23d71459400c26
Shreyansh Singh <singhshreyansh288@gmail.com>
2026-07-06 21:42:48 +0530

    prune

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,13 @@
 "use client";
 
 import HeroSection from "@/components/HeroSection";
-import LogoStripSection from "@/components/LogoStripSection";
+
 import ScrollTextSection from "@/components/ScrollTextSection";
 import AnimatedSection from "@/components/AnimatedSection";
 import DetailsSection from "@/components/DetailsSection";
 import CTASection from "@/components/CTASection";
 import { ScrollTrigger } from "gsap/all";
 import gsap from "gsap";
-import FeaturesSection from "@/components/FeaturesSection";
 
 if (typeof window !== "undefined") {
   gsap.registerPlugin(ScrollTrigger);
@@ -19,7 +18,6 @@ export default function Home() {
     <main className="bg-[#f2f2f2] min-h-screen">
       <HeroSection />
       <ScrollTextSection />
-
       <AnimatedSection />
       <DetailsSection />
       <CTASection />
