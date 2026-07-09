8ce95869158f7f56a23171235e2659c6f7d6f1d9
Shreyansh Singh <singhshreyansh288@gmail.com>
2026-07-09 17:34:54 +0530

    chore: update baseUrl in sitemap to point to vercel.app domain

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next';
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://sherucon.tech';
+  const baseUrl = 'https://sherucon.vercel.app';
 
   return [
     {
